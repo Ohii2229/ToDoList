176e2970403043c1346ff3094fe3ae9c320ebdfa
Hikari Yamaguchi <lightbluetus2456@gmail.com>
2022-08-20 23:03:58 +0900

    アクティビティ・フラグメントと対応するレイアウトを紐付け

---
+++ app/src/main/java/com/android/example/todolist/MainFragment.java
@@ -0,0 +1,12 @@
+package com.android.example.todolist;
+
+class MainFragment : Fragment() {
+
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View?{
+
+        // フラグメントとレイアウトの紐付け！bindingは架け橋だから
+        val binding:FragmentMainBinding=DataBindingUtil.inflate(
+        inflater,R.layout.fragment_main,container,false)
+        }
+}
